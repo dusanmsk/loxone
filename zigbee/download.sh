4b337142c8e0c7ce7c92dc00097a7e4ad6a7d8d9
Dusan Zatkovsky <dusanmsk@gmail.com>
2020-03-12 21:01:24 +0100

    new release

--- zigbee/download.sh
+++ zigbee/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v0.0.1"
+VERSION="v0.0.2"
 
 cd zigbee2mqtt_manager
 rm -rf download; mkdir -p download && cd download && wget https://github.com/dusanmsk/zigbee2mqttmanager/releases/download/${VERSION}/manager-app.jar
